b916ac4ff05abd70bb8d85663e0ffbdf23a9ea49
Mike Huang <wellwind@mail2000.com.tw>
2019-09-04 16:36:23 +0800

    練習 template driven forms 設計

--- advanced/admin/src/app/login/login/login.component.ts
+++ advanced/admin/src/app/login/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgModel, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('emailInput', { static: true }) emailInput: NgModel;
 
-  constructor() { }
+  email = 'test';
+
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {
+
+    console.log(this.emailInput);
+
   }
 
+  login() {
+    console.log(this.emailInput.value);
+
+  }
+
+  submit(event: NgForm) {
+    console.log(event);
+
+    this.httpClient.post('/api', event.value ).subscribe(_ => {
+
+    });
+  }
 }
